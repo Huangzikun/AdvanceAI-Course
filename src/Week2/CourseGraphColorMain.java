e37ddf0bbf74883d9e390c592c9fbafcdd1121f5
huangzikun <381560728@qq.com>
2022-11-06 17:30:38 +0800

    week3

--- src/Week2/CourseGraphColorMain.java
+++ src/Week2/CourseGraphColorMain.java
@@ -5,11 +5,11 @@ import java.util.*;
 /**
  * 1. init the Map. The key is the code of the point, the list is the list of points and lines
  * Map<Integer, ArrayList<Integer>> map = new HashMap<>();
- * {1=[2, 3, 4, 5, 6], 2=[1, 3, 4, 5, 6, 7], 3=[1, 2, 4, 5, 6, 7], 4=[1, 2, 3, 5, 6, 7], 5=[1, 2, 3, 4, 6, 7], 6=[1, 2, 3, 4, 5, 7], 7=[2, 3, 4, 5, 6]}
+ * {3=[1, 2, 4, 5, 6, 7, 8, 9, 10], 2=[1, 3, 4, 5, 6, 7, 9], 1=[2, 3, 4, 5, 6], 4=[1, 2, 3, 5, 7, 10], 5=[1, 2, 3, 4, 6, 7, 9, 10], 6=[1, 2, 3, 5, 7, 8, 9], 7=[2, 3, 4, 5, 6, 8, 9, 10], 8=[3, 6, 7, 9], 9=[2, 3, 5, 6, 7, 8, 10], 10=[3, 4, 5, 7, 9]}
  * <p>
  * 2. sort map by lines number
  * map.sortBylinesNumber();
- * {2=[1, 3, 4, 5, 6, 7], 1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6], 3=[1, 2, 4, 5, 6, 7], 4=[1, 2, 3, 5, 6, 7], 5=[1, 2, 3, 4, 6, 7], 6=[1, 2, 3, 4, 5, 7]}
+ * {5=[1, 2, 3, 4, 6, 7, 9, 10], 2=[1, 3, 4, 5, 6, 7, 9], 1=[2, 3, 4, 5, 6], 4=[1, 2, 3, 5, 7, 10], 6=[1, 2, 3, 5, 7, 8, 9], 7=[2, 3, 4, 5, 6, 8, 9, 10], 8=[3, 6, 7, 9], 9=[2, 3, 5, 6, 7, 8, 10], 10=[3, 4, 5, 7, 9]}
  * <p>
  * 3. check point if the point can use one color, use it and remove it.
  * while(map.notEmpty()) {
@@ -20,77 +20,138 @@ import java.util.*;
  *         }
  *     }
  * }
- * {3=[1, 2, 4, 5, 6, 7], 1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6], 4=[1, 2, 3, 5, 6, 7], 5=[1, 2, 3, 4, 6, 7], 6=[1, 2, 3, 4, 5, 7]}
- * {4=[1, 2, 3, 5, 6, 7], 1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6], 5=[1, 2, 3, 4, 6, 7], 6=[1, 2, 3, 4, 5, 7]}
- * {5=[1, 2, 3, 4, 6, 7], 1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6], 6=[1, 2, 3, 4, 5, 7]}
- * {6=[1, 2, 3, 4, 5, 7], 1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6]}
- * {1=[2, 3, 4, 5, 6], 7=[2, 3, 4, 5, 6]}
- * {1=5, 2=0, 3=1, 4=2, 5=3, 6=4, 7=5}
+ * {5=[1, 2, 3, 4, 6, 7, 9, 10], 2=[1, 3, 4, 5, 6, 7, 9], 1=[2, 3, 4, 5, 6], 4=[1, 2, 3, 5, 7, 10], 6=[1, 2, 3, 5, 7, 8, 9], 7=[2, 3, 4, 5, 6, 8, 9, 10], 8=[3, 6, 7, 9], 9=[2, 3, 5, 6, 7, 8, 10], 10=[3, 4, 5, 7, 9]}
+ * {7=[2, 3, 4, 5, 6, 8, 9, 10], 2=[1, 3, 4, 5, 6, 7, 9], 1=[2, 3, 4, 5, 6], 4=[1, 2, 3, 5, 7, 10], 6=[1, 2, 3, 5, 7, 8, 9], 9=[2, 3, 5, 6, 7, 8, 10], 10=[3, 4, 5, 7, 9]}
+ * {2=[1, 3, 4, 5, 6, 7, 9], 4=[1, 2, 3, 5, 7, 10], 6=[1, 2, 3, 5, 7, 8, 9], 9=[2, 3, 5, 6, 7, 8, 10], 10=[3, 4, 5, 7, 9]}
+ * {6=[1, 2, 3, 5, 7, 8, 9], 4=[1, 2, 3, 5, 7, 10], 9=[2, 3, 5, 6, 7, 8, 10]}
+ * {9=[2, 3, 5, 6, 7, 8, 10]}
 
  so
  q1: The least is 6
  q2:
- 1: S2
- 2: S1, S3
- 3: S4
- 4: S5
- 5: S6
- 7: S7
+ day1: Biology
+ day2: CS,Spanish
+ day3: Math, Psychology
+ day4: English, French
+ day5: Chemistry, Geography
+ day6: History
  */
 public class CourseGraphColorMain {
     public static <string> void main(String[] args) {
 
-        ArrayList<String> S1 = new ArrayList<>() {{
-            add("Math");
-            add("English");
-            add("Biology");
-            add("Chemistry");
+//        ArrayList<String> S1 = new ArrayList<>() {{
+//            add("Math");
+//            add("English");
+//            add("Biology");
+//            add("Chemistry");
+//        }};
+//        ArrayList<String> S2 = new ArrayList<>(){{
+//            add("Math");
+//            add("English");
+//            add("Computer Science");
+//            add("Geography");
+//        }};
+//        ArrayList<String> S3 = new ArrayList<>() {{
+//           add("Biology");
+//           add("Psychology");
+//           add("Geography");
+//           add("Spanish");
+//        }};
+//        ArrayList<String> S4 = new ArrayList<>() {{
+//            add("Biology");
+//            add("Computer Science");
+//            add("History");
+//            add("French");
+//        }};
+//        ArrayList<String> S5 = new ArrayList<>() {{
+//            add("English");
+//            add("Psychology");
+//            add("Computer Science");
+//            add("History");
+//        }};
+//        ArrayList<String> S6 = new ArrayList<>() {{
+//            add("Psychology");
+//            add("Chemistry");
+//            add("Computer Science");
+//            add("French");
+//        }};
+//        ArrayList<String> S7 = new ArrayList<>() {{
+//            add("Psychology");
+//            add("Geography");
+//            add("History");
+//            add("Spanish");
+//        }};
+
+        ArrayList<String> Math = new ArrayList<>() {{
+            add("S1");
+            add("S2");
         }};
-        ArrayList<String> S2 = new ArrayList<>(){{
-            add("Math");
-            add("English");
-            add("Computer Science");
-            add("Geography");
+        ArrayList<String> English = new ArrayList<>() {{
+            add("S1");
+            add("S2");
+            add("S5");
         }};
-        ArrayList<String> S3 = new ArrayList<>() {{
-           add("Biology");
-           add("Psychology");
-           add("Geography");
-           add("Spanish");
+        ArrayList<String> Biology = new ArrayList<>() {{
+            add("S1");
+            add("S3");
+            add("S4");
         }};
-        ArrayList<String> S4 = new ArrayList<>() {{
-            add("Biology");
-            add("Computer Science");
-            add("History");
-            add("French");
+        ArrayList<String> Chemistry = new ArrayList<>() {{
+           add("S1");
+           add("S6");
         }};
-        ArrayList<String> S5 = new ArrayList<>() {{
-            add("English");
-            add("Psychology");
-            add("Computer Science");
-            add("History");
+        ArrayList<String> CS = new ArrayList<>() {{
+            add("S2");
+            add("S4");
+            add("S5");
+            add("S6");
         }};
-        ArrayList<String> S6 = new ArrayList<>() {{
-            add("Psychology");
-            add("Chemistry");
-            add("Computer Science");
-            add("French");
+        ArrayList<String> Geography = new ArrayList<>() {{
+            add("S2");
+            add("S3");
+            add("S7");
         }};
-        ArrayList<String> S7 = new ArrayList<>() {{
-            add("Psychology");
-            add("Geography");
-            add("History");
-            add("Spanish");
+        ArrayList<String> Psychology = new ArrayList<>() {{
+            add("S3");
+            add("S5");
+            add("S6");
+            add("S7");
+        }};
+        ArrayList<String> Spanish = new ArrayList<>() {{
+            add("S3");
+            add("S7");
+        }};
+        ArrayList<String> History = new ArrayList<>() {{
+            add("S4");
+            add("S5");
+            add("S7");
+        }};
+        ArrayList<String> French = new ArrayList<>() {{
+            add("S4");
+            add("S6");
         }};
 
-        ArrayList<ArrayList<String>> list = new ArrayList<>(){{
-            add(S1);
-            add(S2);
-            add(S3);
-            add(S4);
-            add(S5);
-            add(S6);
-            add(S7);
+
+//        ArrayList<ArrayList<String>> list = new ArrayList<>(){{
+//            add(S1);
+//            add(S2);
+//            add(S3);
+//            add(S4);
+//            add(S5);
+//            add(S6);
+//            add(S7);
+//        }};
+        ArrayList<ArrayList<String>> list = new ArrayList<>() {{
+            add(Math);
+            add(English);
+            add(Biology);
+            add(Chemistry);
+            add(CS);
+            add(Geography);
+            add(Psychology);
+            add(Spanish);
+            add(History);
+            add(French);
         }};
 
 
---
+++ src/Week3/Assignment3.md
@@ -0,0 +1,78 @@
+## question
+
+- S1: Math, English, Biology, Chemistry
+- S2: Math, English, Computer Science, Geography
+- S3: Biology, Psychology, Geography, Spanish
+- S4: Biology, Computer Science, History, French
+- S5: English, Psychology, Computer Science, History
+- S6: Psychology, Chemistry, Computer Science, French
+- S7: Psychology, Geography, History, Spanish
+
+Let say, we need to schedule the exams for these students (see assignment 2) within 4 examination days, where each exam day has two exam slots.
+  The following penalty will be imposed if:
+  - Penalty = 2 for each student that need to seat 2 exams consecutively in the same day.
+  - Penalty = 1 for each student that has one slot gap between two exams.
+
+Find an examination timetable that spread the exam for each student across the examination periods (within 4 days), i.e. an exam timetable with minimum penalty. You may use any local search (e.g. Hill Climbing, Simulated Annealing, etc.) to search for a better schedule (just show 2 iterations).
+
+Calculate the quality of the generated timetable.
+
+-----
+| day1.1       | day1.2         | day2.1   | day2.2    | day3.1           | day3.2     | day4.1      | day4.2   |
+|--------------|----------------|----------|-----------|------------------|------------|-------------|----------|
+| Math,History | English,French | Biology  | Chemistry | Computer Science | Geography  | Psychology  | Spanish  |
+
+
+s1 penalty = 2 + 2 = 4
+s2 penalty = 2 + 2 = 4
+s3 penalty = 2
+s4 penalty = 1 + 2 = 3
+s5 penalty = 1
+s6 penalty = 1
+s7 penalty = 2
+all penalty = 4+4+2+3+1+1+2 = 17
+
+First iteration: select one course put it on another slot
+
+| day1.1 | day1.2         | day2.1  | day2.2            | day3.1           | day3.2     | day4.1      | day4.2   |
+|--------|----------------|---------|-------------------|------------------|------------|-------------|----------|
+| Math   | English,French | Biology | Chemistry,History | Computer Science | Geography  | Psychology  | Spanish  |
+
+s1 penalty = 2 + 2 = 4
+s2 penalty = 2 + 2 = 4
+s3 penalty = 2
+s4 penalty = 2
+s5 penalty = 1 + 1 = 2
+s6 penalty = 1 + 1 = 2
+s7 penalty = 1 + 2 = 3
+all penalty = 4+4+2+2+1+2+3 = 18
+so don't select
+
+| day1.1 | day1.2         | day2.1  | day2.2            | day3.1           | day3.2     | day4.1          | day4.2   |
+|--------|----------------|---------|-------------------|------------------|------------|-----------------|----------|
+|        | English,French | Biology | Chemistry,History | Computer Science | Geography  | Psychology,Math | Spanish  |
+
+s1 penalty = 2
+s2 penalty = 2
+s3 penalty = 2
+s4 penalty = 2
+s5 penalty = 1 + 1 = 2
+s6 penalty = 1 + 1 = 2
+s7 penalty = 1 + 2 = 3
+all penalty = 2+2+2+2+2+2+3 = 15
+select this one
+
+Second iteration: select one course put it on another slot
+
+| day1.1     | day1.2         | day2.1  | day2.2            | day3.1           | day3.2     | day4.1 | day4.2   |
+|------------|----------------|---------|-------------------|------------------|------------|--------|----------|
+| Psychology | English,French | Biology | Chemistry,History | Computer Science | Geography  | Math   | Spanish  |
+s1 = 2
+s2 = 2
+s3 = 1 + 1 = 2
+s4 = 2
+s5 = 2
+s6 = 2
+s7 = 1 + 1 = 2
+all = 14
+select this one
