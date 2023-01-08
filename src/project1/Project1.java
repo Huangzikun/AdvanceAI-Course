9f49c90e1d23c65f901df9d9a88d1c4459185f93
huangzikun <381560728@qq.com>
2023-01-08 18:01:06 +0800

    openList

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Project1 {
 
+    static String emptyMeeting = "EMPTY";
+
     static List<String> allMeetings = new ArrayList<>() {{
         add("P1");
         add("P2");
@@ -123,6 +125,57 @@ public class Project1 {
         }});
     }};
 
+    static Map<Integer, ArrayList<String>> researcherDelete = new HashMap<>() {{
+        put(1, new ArrayList<>() {{
+            add("P1");
+            add("P2");
+            add("P3");
+            add("P6");
+        }});
+        put(2, new ArrayList<>() {{
+            add("P2");
+            add("P3");
+        }});
+        put(3, new ArrayList<>() {{
+            add("P2");
+            add("P3");
+            add("P5");
+            add("P8");
+        }});
+        put(4, new ArrayList<>() {{
+            add("P4");
+            add("P6");
+        }});
+        put(5, new ArrayList<>() {{
+            add("P1");
+            add("P6");
+        }});
+        put(6, new ArrayList<>() {{
+            add("P1");
+            add("P3");
+            add("P4");
+            add("P6");
+        }});
+        put(7, new ArrayList<>() {{
+            add("P7");
+            add("P8");
+        }});
+        put(8, new ArrayList<>() {{
+            add("P5");
+            add("P7");
+        }});
+        put(9, new ArrayList<>() {{
+            add("P1");
+            add("P2");
+        }});
+        put(10, new ArrayList<>() {{
+            add("P5");
+            add("P6");
+            add("P7");
+            add("P8");
+        }});
+    }};
+
     /**
      * 3day -> 2slots -> 2room
      */
@@ -186,8 +239,7 @@ public class Project1 {
      * Actual cost
      * 实际代价
      */
-    static Integer G(Map<Integer, Map<Integer, Map<Integer, String>>> arrangeMap) {
-        Integer solution = 0;
+    static Integer G(Map<Integer, Map<Integer, Map<Integer, String>>> arrangeMap, Boolean isEnd) {
 
         ArrayList<String> meetings = new ArrayList<>();
         Map<Integer, LinkedHashSet<String>> currentDayMap = new HashMap<>();
@@ -209,16 +261,52 @@ public class Project1 {
             }
         }
 
+        Iterator<String> iterator = meetings.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            if (emptyMeeting.equals(next)) {
+                iterator.remove();
+            }
+        }
 
         /**
          * cal i: n penalty is given if the researcher is idle (does not attend a meeting
          * between two meetings) for n timeslots (i.e., n=1,2,3,...).
          * 如果研究者在n个时隙（即，n＝1，2，3，…）内空闲（不参加两次会议之间的会议），则给予n个惩罚。
          */
-        for (int i=1; i<=researchers.size(); i++) {
-            for (String s: meetings) {
-                if(!researchers.get(i).contains(s)) {
-                    solution++;
+        Map<Integer, ArrayList<String>> re;
+        if(isEnd) {
+            re = researchers;
+        } else {
+            re =researcherDelete;
+        }
+        Integer i1 = 0;
+
+        i1Over:
+        {
+            for (int day = 1; day <= arrangeMap.size(); day++) {
+                for (int slot = 1; slot <= arrangeMap.get(day).size(); slot++) {
+                    for (int room = 1; room <= arrangeMap.get(day).get(slot).size(); room++) {
+                        String currentMeeting = arrangeMap.get(day).get(slot).get(room);
+                        if (Objects.isNull(currentMeeting)) {
+                            break i1Over;
+                        }
+
+                        for (Integer researcher : re.keySet()) {
+                            List<String> unArrangeMeetings = re.get(researcher);
+                            //安排完了不扣分
+                            if (unArrangeMeetings.isEmpty()) {
+                                continue;
+                            }
+
+                            //如果没有当前的会议就加
+                            if (!unArrangeMeetings.contains(currentMeeting)) {
+                                i1++;
+                            }
+
+                            re.get(researcher).remove(currentMeeting);
+                        }
+                    }
                 }
             }
         }
@@ -228,16 +316,17 @@ public class Project1 {
          * point (whereas he actually has more than one project meeting).
          * 如果研究人员每天只安排一次会议，他们会失去一分
          */
-        for (int i=1; i<=researchers.size(); i++) {
+        Integer i2 = 0;
+        for (int i=1; i<=re.size(); i++) {
             for (int day = 1; day <= arrangeMap.size(); day++) {
                 LinkedHashSet<String> currentDay = currentDayMap.get(day);
 
                 //cross
-                List<String> copy = new ArrayList<>(researchers.get(i));
+                List<String> copy = new ArrayList<>(re.get(i));
                 copy.retainAll(currentDay);
 
                 if(copy.size() == 1) {
-                    solution++;
+                    i2++;
                 }
             }
         }
@@ -246,14 +335,17 @@ public class Project1 {
          * iii: Every day D1 or D2 when a room is unoccupied, there will be a two-point penalty (not reserved for a meeting).
          * 每天D1或D2房间无人时，将处以两分罚款
          */
+        Integer i3 = 0;
         if(currentDayMap.get(1).size() < 4) {
-            solution += 2;
+            i3 += 2;
         }
         if(currentDayMap.get(2).size() < 4) {
-            solution += 2;
+            i3 += 2;
         }
 
-        return solution;
+        System.out.println("i1 = " + i1 + ", i2 = " + i2 + ", i3 = " + i3);
+
+        return i1 + i2 + i3;
     }
 
     /**
@@ -263,7 +355,7 @@ public class Project1 {
     static Map<String, Integer> H(Map<Integer, Map<Integer, Map<Integer, String>>> currentArrangeMap, List<String> openList) {
         Map<String, Integer> solution = new HashMap<>();
 
-        Integer G = G(currentArrangeMap);
+        Integer G = G(currentArrangeMap, false);
 
         Over:
         {
@@ -282,10 +374,12 @@ public class Project1 {
 
                             if(room == 1) {
                                 if(slot == 1) {
-                                    //前一天的最后一个
-                                    D = Math.min(1, day - 1);
-                                    R = 2;
-                                    S = 2;
+                                    if(D > 1) {
+                                        //前一天的最后一个
+                                        D = Math.min(1, day - 1);
+                                        R = 2;
+                                        S = 2;
+                                    }
                                 } else {
                                     S = Math.min(1, slot - 1);
                                     R = 2;
@@ -296,15 +390,17 @@ public class Project1 {
 
                             String lastMeeting = arrangeMap.get(D).get(S).get(R);
 
+                            /**
+                             * 每个会议搜索一次
+                             */
                             for (String meeting : openList) {
 
                                 Map<Integer, Map<Integer, Map<Integer, String>>> testMap = new HashMap<>(currentArrangeMap);
                                 testMap.get(day).get(slot).put(room, meeting);
-                                Integer testG = G(testMap);
                                 /**
                                  * 已知如果这么安排会增加的罚分
                                  */
-                                Integer addSol = testG - G;
+                                Integer addSol = G(testMap, false);
 
                                 /**
                                  * 预测如果这样安排会有多少罚分
@@ -328,7 +424,39 @@ public class Project1 {
                                     addSol += A.size();
                                 }
 
+                                /**
+                                 * TODO 猜的一个值
+                                 */
+                                addSol /= 4;
 
+                                /**
+                                 * ii
+                                 */
+                                for(List<String> meetings : researchers.values()){
+                                    if(meetings.contains(meeting)) {
+                                        addSol += 2;
+                                    }
+                                }
+
+                                /**
+                                 * iii
+                                 */
+                                //D1 & D2
+                                for (int dd = 1; dd <= 2; dd++) {
+
+                                    DDD:
+                                    {
+                                        for (int ss = 1; ss <= testMap.get(dd).size(); ss++) {
+                                            for (int rr = 1; rr <= testMap.get(dd).get(ss).size(); rr++) {
+                                                if (Objects.isNull(testMap.get(dd).get(ss).get(rr))) {
+                                                    addSol += 2;
+
+                                                    break DDD;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
 
                                 solution.put(meeting, addSol);
                             }
@@ -411,27 +539,64 @@ public class Project1 {
         return openList;
     }
 
-    public static Integer eval(Map<Integer, Map<Integer, List<Integer>>> arrangeMap) {
+    public static String eval(Map<Integer, Map<Integer, Map<Integer, String>>> arrangeMap, List<String> openList) {
+        Integer gEval = G(arrangeMap, false);
 
+        Map<String, Integer> solList = H(arrangeMap, openList);
+        System.out.println("sol list = " + solList);
 
+        String meeting = null;
+        Integer sol = gEval;
 
-        return 0;
+        for(String mapKey : solList.keySet()){
+            if(Objects.isNull(meeting) || solList.get(mapKey) < sol) {
+                meeting = mapKey;
+                sol = solList.get(mapKey);
+            }
+        }
+
+        System.out.println("sol = " + sol + ", meeting = " + meeting);
+        return meeting;
     }
 
     public static <string> void main(String[] args) {
-        Map<String, ArrayList<String>> sort = sortMap(graph);
 
-        System.out.println(sort);
+        List<String> allList = new ArrayList<>(allMeetings);
+        Integer D = 1;
+        Integer S = 1;
+        Integer R = 1;
 
-        arrangeMap.get(1).get(1).put(1, "P1");
+        while(!allList.isEmpty()) {
 
-        Integer s = G(arrangeMap);
-        System.out.println(s);
+            List<String> openList = openList(arrangeMap);
+            String meeting = null;
 
-        List<String> openList = openList(arrangeMap);
-        System.out.println(openList);
+            if(!openList.isEmpty()) {
+                meeting = eval(arrangeMap, openList);
+                for (List<String> researcherList : researcherDelete.values()) {
+                    researcherList.remove(meeting);
+                }
 
-        H(arrangeMap, openList);
+            } else {
+                meeting = emptyMeeting;
+            }
 
+            arrangeMap.get(D).get(S).put(R, meeting);
+            System.out.println("select meeting = " + meeting + ", arrange = " + arrangeMap);
+            allList.remove(meeting);
+
+            if(R == 1) {
+                R = 2;
+            } else if (S == 1) {
+                S = 2;
+                R = 1;
+            } else {
+                D ++;
+                S = 1;
+                R = 1;
+            }
+        }
+
+        System.out.println("result score = " + G(arrangeMap, true));
     }
 }
